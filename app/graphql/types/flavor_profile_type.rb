0bac69b596d09f700b6202ec0697e6baeebe60c8
Stefan Bloom <stefanjbloom88@gmail.com>
2025-01-30 16:13:56 -0700

    feat: graphql type/objects generated for tables

---
+++ app/graphql/types/flavor_profile_type.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Types
+  class FlavorProfileType < Types::BaseObject
+  end
+end
---
+++ app/graphql/types/recipe_category_type.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Types
+  class RecipeCategoryType < Types::BaseObject
+  end
+end
