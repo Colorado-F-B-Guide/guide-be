2f57b0a678b40c9355705336e9cff79d27429102
Stefan Bloom <stefanjbloom88@gmail.com>
2025-01-30 16:42:26 -0700

    feat: graphql types defined

--- app/graphql/types/brewery_type.rb
+++ app/graphql/types/brewery_type.rb
@@ -2,5 +2,12 @@
 
 module Types
   class BreweryType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :location, String, null: false
+    field :founded_date, Date, null: true
+    field :website_url, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
--- app/graphql/types/recipe_type.rb
+++ app/graphql/types/recipe_type.rb
@@ -2,5 +2,15 @@
 
 module Types
   class RecipeType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :description, String, null: true
+    field :image_url, String, null: true
+    field :prep_time, Integer, null: true
+    field :cook_time, Integer, null: true
+    field :difficulty, String, null: true
+    field :chef_notes, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
